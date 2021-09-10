2add2ec77ee21d8c0558472454b365fcb8af06e8
julian david suarez rendon <38923604+davig3t3@users.noreply.github.com>
2021-09-10 13:55:09 -0500

    Agenda telefonica

--- src/taller/AgendaTelefonica/app/App.java
+++ src/taller/AgendaTelefonica/app/App.java
@@ -1,7 +1,28 @@
 package taller.AgendaTelefonica.app;
 
+import taller.AgendaTelefonica.dominio.Agenda;
+import taller.AgendaTelefonica.dominio.Contacto;
+
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) {
+
+        Agenda agenda = new Agenda("qwerty");
+
+        Contacto contacto = new Contacto("Arturo", "Perez",2341432);
+        Contacto contacto2 = new Contacto("Kevin", "Ramirez",243334);
+        Contacto contacto3 = new Contacto("Sara", "Mesa", 43245675);
+        Contacto contacto4 = new Contacto("Andres", "Quintero",5978654);
+
+        agenda.agregar(contacto);
+        agenda.agregar(contacto2);
+        agenda.agregar(contacto3);
+        agenda.agregar(contacto4);
+
+        agenda.buscar("Kevin","Ramirez");
+        agenda.buscar(243334);
+
 
     }
 }
---
+++ src/taller/AgendaTelefonica/dominio/Agenda.java
@@ -0,0 +1,75 @@
+package taller.AgendaTelefonica.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class Agenda {
+
+    public static final int CANT_MAX_CONT = 20;
+    private String directorio;
+    private List<Contacto> contactos;
+
+    public Agenda(String directorio){
+        this.directorio = directorio;
+        this.contactos = new ArrayList<>();
+    }
+
+    public Contacto buscar(String nombre, String apellido){
+        Contacto contactoBuscado = null;
+        for(Contacto contacto: this.contactos){
+            if(contacto.getNombre().equalsIgnoreCase(nombre)){
+                contactoBuscado = contacto;
+            }
+            if(contacto.getApellido().equalsIgnoreCase(apellido)){
+                contactoBuscado = contacto;
+            }
+        }
+        return contactoBuscado;
+    }
+
+    public Contacto buscar(int numero){
+
+        Contacto contactoBuscado = null;
+
+        for (Contacto contacto: this.contactos) {
+            if (contacto.getNumero() == numero) {
+                contactoBuscado = contacto;
+                break;
+            }
+        }
+
+        return contactoBuscado;
+
+    }
+
+    public boolean agregar(Contacto contacto){
+        if (this.contactos.size() < CANT_MAX_CONT){
+            Contacto c = buscar(contacto.getNumero());
+            if (c == null) {
+                this.contactos.add(contacto);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public  boolean eliminar(Contacto contacto){
+        Contacto c = buscar(contacto.getNumero());
+        if (c == null){
+            this.contactos.remove(contacto);
+            return true;
+        } else {
+            return  false;
+        }
+    }
+
+
+
+
+
+
+}
