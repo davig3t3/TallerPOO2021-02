cc8661930296f79ee24cd42343383e32dd462d4b
julian david suarez rendon <38923604+davig3t3@users.noreply.github.com>
2021-11-19 17:44:34 -0500

    Oftalmologo v3

--- src/taller/Oftalmologo/app/AppOftalmologo.java
+++ src/taller/Oftalmologo/app/AppOftalmologo.java
@@ -8,19 +8,19 @@ public class AppOftalmologo {
     public static void main(String[] args) {
         Oftalmologo oftalmologo = new Oftalmologo("PEPE");
 
-        PacienteApto pacienteApto = new PacienteApto("Arturo",39,false);
+        /*PacienteApto pacienteApto = new PacienteApto("Arturo",39,false);
         PacienteNoApto pacienteNoApto = new PacienteNoApto("Jose Alberto",55,false);
         PacienteApto pacienteApto2 = new PacienteApto("Miguel",60,false);
         PacienteNoApto pacienteNoApto2 = new PacienteNoApto("Sara",23,true);
+         */
+        //oftalmologo.addPaciente(pacienteApto,pacienteApto2,pacienteNoApto,pacienteNoApto2);
+        oftalmologo.revisarPaciente("Arturo",34);
+        //oftalmologo.revisarPaciente(pacienteNoApto);
+        //oftalmologo.revisarPaciente(pacienteApto2);
+        //oftalmologo.revisarPaciente(pacienteNoApto2);
 
-        oftalmologo.addPaciente(pacienteApto,pacienteApto2,pacienteNoApto,pacienteNoApto2);
-        oftalmologo.revisarPaciente(pacienteApto);
-        oftalmologo.revisarPaciente(pacienteNoApto);
-        oftalmologo.revisarPaciente(pacienteApto2);
-        oftalmologo.revisarPaciente(pacienteNoApto2);
-
-        oftalmologo.operarPaciente();
-        oftalmologo.getPacientesAOperar();
+        //oftalmologo.operarPaciente();
+        //oftalmologo.getPacientesAOperar();
 
 
 
--- src/taller/Oftalmologo/dominio/Oftalmologo.java
+++ src/taller/Oftalmologo/dominio/Oftalmologo.java
@@ -9,31 +9,51 @@ import java.util.Locale;
 public class Oftalmologo {
     private String nombre;
     private List<Paciente> pacientes;
+    private List<PacienteApto> pacienteAptos;
     public static final int EDAD_MAX = 40;
 
     public Oftalmologo(String nombre) {
         this.nombre = nombre;
         this.pacientes = new ArrayList<>();
+        this.pacienteAptos = new ArrayList<>();
     }
 
-    public void addPaciente(Paciente... p) {
+    /*public void addPaciente(Paciente... p) {
 
         this.pacientes.addAll(Arrays.asList(p));
 
     }
 
-    public void revisarPaciente(Paciente paciente) {
+     */
 
-        if (paciente.getNombre().toLowerCase(Locale.ROOT).contains("a") && paciente.getEdad() < EDAD_MAX) {
+    public Paciente revisarPaciente(String nombre, int edad) {
+
+
+        Paciente paciente = null;
+
+        if (nombre.toLowerCase(Locale.ROOT).contains("a")) {
             System.out.println(paciente.getNombre() + " Necesita cirugia");
+        }
+        if (nombre.toLowerCase(Locale.ROOT).contains("a") && edad < EDAD_MAX) {
+            System.out.println(paciente.getNombre() + " El paciente es apto");
+            paciente = new PacienteApto(nombre, edad, true);
+            pacientes.add(paciente);
         } else {
             System.out.println(paciente.getNombre() + " El paciente no cumple con los requisitos para ser operado");
+            paciente = new PacienteNoApto(nombre, edad, false);
         }
+
+        return paciente;
     }
 
-    public void operarPaciente() {
+
+    public void operarPacientes() {
 
         pacientes.stream().filter(p -> p instanceof Operable).forEach(paciente -> {
+            ((Operable) paciente).operar();
+        });
+
+        /*pacientes.stream().filter(p -> p instanceof Operable).forEach(paciente -> {
             if (paciente.getNombre().toLowerCase(Locale.ROOT).contains("a") && paciente.getEdad() < EDAD_MAX) {
                 System.out.println(paciente.getNombre() + " El paciente sera intervenido");
                 ((Operable) paciente).operar();
@@ -41,19 +61,22 @@ public class Oftalmologo {
                 System.out.println(paciente.getNombre() + " El paciente no sera intervenido");
             }
         });
+
+         */
+
+
     }
 
     public void getPacientesAOperar() {
 
         pacientes.stream().filter(p -> p instanceof Operable).forEach(paciente -> {
-            if (paciente.getNombre().toLowerCase(Locale.ROOT).contains("a") && paciente.getEdad() < EDAD_MAX) {
-                if (paciente.isNecesitaCirugia() == false) {
-                    paciente.necesitaCirugia = true;
-                } else {
-                    System.out.println("El paciente ya fue intervenido");
-                }
-                System.out.println("Los pacientes que seran intervenidos son: " + paciente.getNombre());
+            if (paciente.isNecesitaCirugia() == false) {
+                paciente.necesitaCirugia = false;
+                pacienteAptos.add((PacienteApto) paciente);
+            } else {
+                System.out.println("El paciente ya fue intervenido");
             }
+            System.out.println("Los pacientes que seran intervenidos son: " + paciente.getNombre());
         });
 
     }
